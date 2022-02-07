e13fafa23aaf196d4369e42408c9de4b424e2c1b
GGolfz <wisarutgolf11@gmail.com>
2022-02-07 14:07:25 +0700

    add: render board :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {
+  triplePiece,
+  tripleEquation,
+  doublePiece,
+  doubleEquation,
+} from "./data";
+import { BONUS } from "./constant";
+import "./App.css";
 
 function App() {
+  const [board, setBoard] = useState([]);
+  useEffect(() => {
+    setupBoard();
+  }, []);
+  const setupBoard = () => {
+    let board = [];
+    for (let i = 0; i < 15; i++) {
+      let temp = [];
+      for (let j = 0; j < 15; j++) {
+        temp.push({
+          row: i,
+          col: j,
+          piece: null,
+        });
+      }
+      board.push(temp);
+    }
+    for (let pos of triplePiece) {
+      board[pos.row][pos.col] = {
+        ...board[pos.row][pos.col],
+        bonus: BONUS.TRIPLE_PIECE,
+      };
+    }
+    for (let pos of tripleEquation) {
+      board[pos.row][pos.col] = {
+        ...board[pos.row][pos.col],
+        bonus: BONUS.TRIPLE_EQUATION,
+      };
+    }
+    for (let pos of doublePiece) {
+      board[pos.row][pos.col] = {
+        ...board[pos.row][pos.col],
+        bonus: BONUS.DOUBLE_PIECE,
+      };
+    }
+    for (let pos of doubleEquation) {
+      board[pos.row][pos.col] = {
+        ...board[pos.row][pos.col],
+        bonus: BONUS.DOUBLE_EQUATION,
+      };
+    }
+    setBoard(board);
+  };
+  const getPieceClass = (bonus) => {
+    return bonus ? `position piece-${bonus?.toLowerCase().replace("_","-")}` : "position";
+  };
+  const getBonusText = (bonus) => {
+    let b = bonus.split("_");
+    if(b[0] == "TRIPLE") {
+      b[2] = "3X"
+    } else if(b[0] == "DOUBLE"){
+      b[2] = "2X"
+    }
+    return <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+      <span style={{fontSize: '0.7rem'}}>{b[0]}</span>
+      <span style={{fontSize: '1.2rem'}}>{b[2]}</span>
+      <span style={{fontSize: '0.7rem'}}>{b[1]}</span>
+    </div>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="board">
+        {board.map((row, i) => {
+          return (
+            <div key={i} className="row">
+              {row.map((col, j) => {
+                return (
+                  <div key={j} className={getPieceClass(col.bonus)}>
+                    {col.piece ? col.piece : col.bonus ? getBonusText(col.bonus) : ""}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
