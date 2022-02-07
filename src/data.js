d3d49bcf47020dfd7c5ae8d78aebf64fa13eec89
GGolfz <wisarutgolf11@gmail.com>
2022-02-07 14:07:19 +0700

    add: board data :sparkles:

---
+++ src/data.js
@@ -0,0 +1,254 @@
+const tripleEquation = [
+  {
+    row: 0,
+    col: 0,
+  },
+  {
+    row: 0,
+    col: 7,
+  },
+  {
+    row: 0,
+    col: 14,
+  },
+  {
+    row: 7,
+    col: 0,
+  },
+  {
+    row: 7,
+    col: 14,
+  },
+  {
+    row: 14,
+    col: 0,
+  },
+  {
+    row: 14,
+    col: 7,
+  },
+  {
+    row: 14,
+    col: 14,
+  },
+];
+
+const triplePiece = [
+  {
+    row: 1,
+    col: 5,
+  },
+  {
+    row: 1,
+    col: 9,
+  },
+  {
+    row: 4,
+    col: 4,
+  },
+  {
+    row: 4,
+    col: 10,
+  },
+  {
+    row: 5,
+    col: 1,
+  },
+  {
+    row: 5,
+    col: 5,
+  },
+  {
+    row: 5,
+    col: 9,
+  },
+  {
+    row: 5,
+    col: 13,
+  },
+  {
+    row: 7,
+    col: 7,
+  },
+  {
+    row: 9,
+    col: 1,
+  },
+  {
+    row: 9,
+    col: 5,
+  },
+  {
+    row: 9,
+    col: 9,
+  },
+  {
+    row: 9,
+    col: 13,
+  },
+  {
+    row: 10,
+    col: 4,
+  },
+  {
+    row: 10,
+    col: 10,
+  },
+  {
+    row: 13,
+    col: 5,
+  },
+  {
+    row: 13,
+    col: 9,
+  },
+];
+
+const doubleEquation = [
+  {
+    row: 1,
+    col: 1,
+  },
+  {
+    row: 2,
+    col: 2,
+  },
+  {
+    row: 3,
+    col: 3,
+  },
+  {
+    row: 1,
+    col: 13,
+  },
+  {
+    row: 2,
+    col: 12,
+  },
+  {
+    row: 3,
+    col: 11,
+  },
+  {
+    row: 11,
+    col: 3,
+  },
+  {
+    row: 12,
+    col: 2,
+  },
+  {
+    row: 13,
+    col: 1,
+  },
+  {
+    row: 11,
+    col: 11,
+  },
+  {
+    row: 12,
+    col: 12,
+  },
+  { row: 13, col: 13 },
+];
+
+const doublePiece = [
+  {
+    row: 0,
+    col: 3,
+  },
+  {
+    row: 0,
+    col: 11,
+  },
+  {
+    row: 2,
+    col: 6,
+  },
+  {
+    row: 2,
+    col: 8,
+  },
+  {
+    row: 3,
+    col: 0,
+  },
+  {
+    row: 3,
+    col: 7,
+  },
+  {
+    row: 3,
+    col: 14,
+  },
+  {
+    row: 6,
+    col: 2,
+  },
+  {
+    row: 6,
+    col: 6,
+  },
+  {
+    row: 6,
+    col: 8,
+  },
+  {
+    row: 6,
+    col: 12,
+  },
+  {
+    row: 7,
+    col: 3,
+  },
+  {
+    row: 7,
+    col: 11,
+  },
+  {
+    row: 8,
+    col: 2,
+  },
+  {
+    row: 8,
+    col: 6,
+  },
+  {
+    row: 8,
+    col: 8,
+  },
+  {
+    row: 8,
+    col: 12,
+  },
+  {
+    row: 11,
+    col: 0,
+  },
+  {
+    row: 11,
+    col: 7,
+  },
+  {
+    row: 11,
+    col: 14,
+  },
+  {
+    row: 12,
+    col: 6,
+  },
+  {
+    row: 12,
+    col: 8,
+  },
+  {
+    row: 14,
+    col: 3,
+  },
+  {
+    row: 14,
+    col: 11,
+  },
+];
+
+export { tripleEquation, triplePiece, doubleEquation, doublePiece };
